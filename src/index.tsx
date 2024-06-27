23e2c757099915d1f02cb483a193c240b3bb18b9
PigHero82 <wirya_tama99@hotmail.com>
2024-06-27 20:12:24 +0800

    chore: add tailwind, daisyui & react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// React
+import React from 'react'
+import reportWebVitals from './reportWebVitals'
+
+// Style
+import './index.css'
+
+// Third-Party Libraries
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// Route
+import { route } from './route'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={createBrowserRouter(route)} />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
---
+++ src/route.tsx
@@ -0,0 +1,12 @@
+// Pages
+import App from "./App"
+
+// Types
+import type { RouteObject } from "react-router"
+
+export const route: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />
+  }
+]
